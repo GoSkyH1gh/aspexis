640a0fcc5950e56546b7833f06b946a2c0f2d795
GoSkyH1gh <serbangalea1@gmail.com>
2026-04-03 18:58:04 +0300

    add rankings to api & improve frontend wynncraft guild display

--- frontend/src/pages/playerComponents/wynncraftGuild.tsx
+++ frontend/src/pages/playerComponents/wynncraftGuild.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { motion } from "motion/react";
 import InfoCard from "./infoCard";
 import { formatISOTimestamp, formatValue } from "../../utils/utils";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { ToggleGroup } from "radix-ui";
 import { WynncraftGuildInfo } from "../../client";
 
 function WynncraftGuild({
@@ -12,38 +14,86 @@ function WynncraftGuild({
   if (wynncraftGuildData === null) {
     return <>No Guild Data to show</>;
   }
-  let navigator = useNavigate();
+
+  const [displayMode, setDisplayMode] = useState<"card" | "list">("card");
+
   const guildMemberElements = wynncraftGuildData.members.map((member) => {
-    return (
-      <li>
-        <motion.button
-          className="wynn-guild-member-item"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          onClick={() => navigator(`/player/${member.uuid}`)}
-          key={member.uuid}
-        >
-          <div className="guild-member-flex-container">
-            <img
-              src={`https://minotar.net/helm/${member.uuid}/128.png`}
-              className="guild-member-image"
-              height="80"
-              width="80"
-              loading="lazy"
-            />
-            <div>
-              <p className="em-text list-username">{member.username}</p>
-              <p className="secondary-text left-align">
-                {member.rank}
-                <br />
-                joined {formatISOTimestamp(member.joined)}
+    if (displayMode === "card") {
+      return (
+        <li key={`${member.uuid}:card`}>
+          <Link to={`/player/${member.uuid}`}>
+            <motion.div
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ duration: 0.1, ease: "easeInOut" }}
+              className="guild-list-item"
+              variants={{
+                hidden: { opacity: 0 },
+                show: { opacity: 1 },
+              }}
+              tabIndex={-1}
+            >
+              <div className="guild-member-flex-container">
+                <img
+                  src={`https://minotar.net/helm/${member.uuid}/128.png`}
+                  className="guild-member-image"
+                  alt={`head of ${member.username}'s skin`}
+                  loading="lazy"
+                />
+                <div className="guild-member-item-card">
+                  <div>
+                    <p className="list-username">{member.username}</p>
+                    <p className="list-rank">{member.rank}</p>
+                  </div>
+
+                  <p className="list-uuid">
+                    Joined {formatISOTimestamp(member.joined)}
+                  </p>
+                </div>
+              </div>
+            </motion.div>
+          </Link>
+        </li>
+      );
+    }
+    if (displayMode === "list") {
+      return (
+        <li key={`${member.uuid}:list`}>
+          <Link to={`/player/${member.uuid}`}>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.1, ease: "easeInOut" }}
+              className="hypixel-guild-list-item-list"
+              variants={{
+                hidden: { opacity: 0 },
+                show: { opacity: 1 },
+              }}
+            >
+              <div className="hypixel-guild-list-member-primary">
+                <img
+                  src={`https://minotar.net/helm/${member.uuid}/128.png`}
+                  className="guild-member-image guild-member-image-list"
+                  alt={`head of ${member.username}'s skin`}
+                  loading="lazy"
+                />
+                <div className="flex">
+                  <p className="list-username">{member.username}</p>
+                  <p className="list-rank">{member.rank}</p>
+                </div>
+              </div>
+              <p className="list-date-joined">
+                Joined {formatISOTimestamp(member.joined)}
               </p>
-            </div>
-          </div>
-        </motion.button>
-      </li>
-    );
+            </motion.div>
+          </Link>
+        </li>
+      );
+    }
   });
+
   return (
     <>
       <ul className="info-card-list">
@@ -62,8 +112,39 @@ function WynncraftGuild({
           value={formatValue(wynncraftGuildData.member_count)}
         />
       </ul>
-      <p>{wynncraftGuildData.name}'s members</p>
-      <ul className="wynn-guild-member-list">{guildMemberElements}</ul>
+
+      <div className="hypixel-guild-members-heading">
+        <h3>{wynncraftGuildData.name}'s members</h3>
+        <div className="flex">
+          Display as
+          <ToggleGroup.Root
+            type="single"
+            value={displayMode}
+            onValueChange={(value) => {
+              if (value) setDisplayMode(value as "card" | "list");
+            }}
+            className="select-container"
+          >
+            <ToggleGroup.Item
+              value="card"
+              className={`select-button-left select-button ${displayMode === "card" && "select-button-active"}`}
+            >
+              Card
+            </ToggleGroup.Item>
+            <ToggleGroup.Item
+              value="list"
+              className={`select-button-right select-button ${displayMode === "list" && "select-button-active"}`}
+            >
+              List
+            </ToggleGroup.Item>
+          </ToggleGroup.Root>
+        </div>
+      </div>
+      <ul
+        className={`guild-list ${displayMode === "card" ? "guild-list-card" : "guild-list-list"}`}
+      >
+        {guildMemberElements}
+      </ul>
     </>
   );
 }
